29fd5e7d753d00768bbccbeca873dcd196734fa5
Val Brisendine <vsbrisendine@gmail.com>
2022-02-09 10:36:01 -0600

    styling and fixing buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Forecast from "./components/Forecast/Forecast";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+   <div className="App">
+     <header className="App-header">
+       <h1>React Weather App</h1>
+     </header>
+     <main>
+       <Forecast />
+     </main>
+      <footer>
+        App created by VS Brisendine
+      </footer>
     </div>
   );
 }
